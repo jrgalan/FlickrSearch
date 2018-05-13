484776d3635c14628e175e81ac471ff68696be25
Jensen Galan <jrgalan@yahoo.com>
2018-05-13 10:17:31 -1000

    Break out unit tests for View Model into separate class.

--- FlickrSearchTests/FlickrSearchTests.swift
+++ FlickrSearchTests/FlickrSearchTests.swift
@@ -24,7 +24,6 @@ class FlickrSearchTests: XCTestCase {
         """.data(using: .utf8)!
     
     var photosResponse: PhotosResponse?
-    var photosViewModel: PhotosViewModel!
     
     override func setUp() {
         super.setUp()
@@ -33,13 +32,10 @@ class FlickrSearchTests: XCTestCase {
         } catch let jsonError {
             XCTFail("Decoding error: \(jsonError.localizedDescription)")
         }
-        photosViewModel = PhotosViewModel(flickrAPIManager: MockFlickrAPIManager())
-        photosViewModel.performSearch(searchTerm: "Bleacher Report") { success in }
     }
     
     override func tearDown() {
         photosResponse = nil
-        photosViewModel = nil
         super.tearDown()
     }
     
@@ -99,55 +95,4 @@ class FlickrSearchTests: XCTestCase {
         }
     }
     
-    func testNumberOfPhotos() {
-        let numberOfPhotos = photosViewModel.numberOfPhotos
-        XCTAssertEqual(numberOfPhotos, 1, "PhotosViewModel numberOfPhotos wrong")
-    }
-    
-    func testPhotoTitle() {
-        let photo = photosViewModel.photo(for: 0)
-        XCTAssertEqual(photo?.title, "Buick re-hires Tiger Woods", "PhotosViewModel photo title wrong")
-    }
-    
-    func testClearPhotos() {
-        photosViewModel.clearCurrentPhotos()
-        let numberOfPhotos = photosViewModel.numberOfPhotos
-        XCTAssertEqual(numberOfPhotos, 0, "PhotosViewModel numberOfPhotos wrong after clearing")
-    }
-    
-    func testPerformSearch() {
-        photosViewModel.performSearch(searchTerm: "Simms & Lefkoe") { success in }
-        let numberOfPhotos = photosViewModel.numberOfPhotos
-        XCTAssertEqual(numberOfPhotos, 1, "PhotosViewModel numberOfPhotos wrong after searching")
-    }
-    
 }
-
-class MockFlickrAPIManager: FlickrAPIManager {
-    
-    let photoResponseJSON = """
-        {
-        "photos":
-            {"page":1,"pages":10106,"perpage":25,"total":"252644","photo":
-                [
-                    {"id":"26777141167","owner":"159226766@N06","secret":"f21dd5715f","server":"825","farm":1,"title":"Buick re-hires Tiger Woods","ispublic":1,"isfriend":0,"isfamily":0}
-                ]
-            }
-        ,"stat":"ok"
-        }
-        """.data(using: .utf8)!
-    
-    override func searchPhotos(page: Int, searchTerm: String, completion: @escaping (Photos?, Error?) -> Void) {
-        do {
-            let photosResponse = try JSONDecoder().decode(PhotosResponse.self, from: photoResponseJSON)
-            if let photos = photosResponse.photos {
-                completion(photos, nil)
-            } else {
-                completion(nil, FlickrAPIError.noPhotos)
-            }
-        } catch let jsonError {
-            XCTFail("Decoding error: \(jsonError.localizedDescription)")
-        }
-    }
-}
-
